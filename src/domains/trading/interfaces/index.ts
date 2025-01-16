38ef1cfca1ceb3bf3581959e617a3011709336c6
François Schuers <francois.schuers@gmail.com>
2025-01-16 14:59:36 +0100

    feat: add controller and module

---
+++ src/domains/trading/interfaces/index.ts
@@ -0,0 +1,6 @@
+import { ITradingInformation } from './trading-information.interface';
+export type { ITradingInformation };
+
+export const TradingInformationTokens = {
+  TradingInformation: Symbol('ITradingInformation'),
+} as const;
