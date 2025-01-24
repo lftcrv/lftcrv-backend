1f0da87446424da8b44589db52483b5fc5d184a3
François Schuers <francois.schuers@gmail.com>
2025-01-24 10:12:19 +0100

    fix: add missing controller declaration

--- src/domains/agent-token/agent-token.module.ts
+++ src/domains/agent-token/agent-token.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AgentTokenTokens } from './interfaces';
 import { CreateAgentTokenService } from './services/create-agent-token.service';
 import { QueryAgentTokenService } from './services/query-agent-token.service';
+import { AgentTokenController } from './agent-token.controller';
 
 @Module({
+  controllers: [AgentTokenController],
   providers: [
     {
       provide: AgentTokenTokens.CreateAgentToken,
