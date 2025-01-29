4cf73862e06eee947156ff6151f46f149698ec37
LouGel <l.gelinet@protonmail.ch>
2025-01-29 18:23:57 +0100

    chore: price pushing route

--- src/domains/agent-token/interfaces/query-agent-token.interface.ts
+++ src/domains/agent-token/interfaces/query-agent-token.interface.ts
@@ -1,5 +1,6 @@
 export interface IQueryAgentToken {
   simulateBuy(agentId: string, tokenAmount: bigint): Promise<bigint>;
+  getCurrentPrice(agentId: string): Promise<bigint>;
   simulateSell(agentId: string, tokenAmount: bigint): Promise<bigint>;
   bondingCurvePercentage(agentId: string): Promise<number>;
 }
