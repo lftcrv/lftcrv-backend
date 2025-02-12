13c1c1d92f8b21a6ae4435453196ef285f98d945
rmzlb <ramzi.laieb@gmail.com>
2025-02-12 10:10:14 +0100

    fix/leaderboardmodule
    remove prisma import as already available in global()

--- src/domains/leaderboard/leaderboard.module.ts
+++ src/domains/leaderboard/leaderboard.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { LeaderboardController } from './leaderboard.controller';
-import { PrismaModule } from '../../shared/prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
   controllers: [LeaderboardController],
 })
 export class LeaderboardModule {}
