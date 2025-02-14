103462ed2eaab08fa4ca9e9d1b287b3dbea54c2a
julienbrs <julien.bourseau@gmail.com>
2025-02-14 12:36:54 +0100

    revert

--- src/shared/blockchain/services/abi.service.ts
+++ src/shared/blockchain/services/abi.service.ts
@@ -1,47 +1,19 @@
-import { Inject, Injectable, Logger } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { BlockchainTokens, IAbiService, IProviderService } from '../interfaces';
 import { Abi } from 'starknet';
 
 @Injectable()
 export class AbiService implements IAbiService {
-  private readonly logger = new Logger(AbiService.name);
-
   constructor(
     @Inject(BlockchainTokens.Provider)
     private readonly providerService: IProviderService,
   ) {}
+  private abi: Abi;
 
-  async getAbi(contractAddress: string): Promise<Abi> {
-    try {
-      let contractAddress = "0x0044fb6d9a2b7a6518d3460b613ee376c4b364cb015844cee6559a8cdd5c9072";
-      const provider = this.providerService.getProvider();
-      this.logger.log(`Getting ABI for contract: ${contractAddress}`);
+  async getAbi(contractAddress: string): Promise<Abi> { // todo stop spamming that, should be called once per contract and stored
+    const provider = this.providerService.getProvider();
+    const { abi } = await provider.getClassAt(contractAddress);
 
-      const classHash = await provider.getClassHashAt(contractAddress);
-      this.logger.log(
-        `Class hash for contract ${contractAddress}: ${classHash}`,
-      );
-
-      const contractClass = await provider.getClass(classHash);
-      this.logger.log('Contract class response:', contractClass);
-
-      if (!contractClass) {
-        this.logger.error(`No contract class found for hash ${classHash}`);
-        throw new Error(`No contract class found for hash ${classHash}`);
-      }
-
-      if (!contractClass.abi) {
-        this.logger.error(
-          'Contract class structure:',
-          JSON.stringify(contractClass, null, 2),
-        );
-        throw new Error(`No ABI found in contract class for hash ${classHash}`);
-      }
-
-      return contractClass.abi;
-    } catch (error) {
-      this.logger.error(`Error in getAbi for ${contractAddress}:`, error);
-      throw error;
-    }
+    return abi;
   }
 }
