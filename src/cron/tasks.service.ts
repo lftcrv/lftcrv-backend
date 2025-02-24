fc3eb1926c8f4b038bba1578ae399b52d75c2c74
julienbrs <julien.bourseau@gmail.com>
2025-02-24 11:41:57 -0700

    change some timeframe config

--- src/cron/tasks.service.ts
+++ src/cron/tasks.service.ts
@@ -37,7 +37,7 @@ export class TasksService {
     }
   }
 
-  @Cron(CronExpression.EVERY_5_MINUTES)
+  @Cron(CronExpression.EVERY_10_MINUTES)
   async sendActOnParadexMessage() {
     const startTime = Date.now();
     this.logger.log('🚀 Sending "EXECUTE ACT_ON_PARADEX" to active agents');
